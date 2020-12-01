20ab8dd46ab8aa021b5ce538d40b1eea5269ef54
jordanstrobing <jordanstrobing@localhost>
2020-11-30 20:16:20 -0500

    Second commit with loop fix!

--- creditcard/src/creditcard.java
+++ creditcard/src/creditcard.java
@@ -5,27 +5,32 @@ public class creditcard {
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		
-		System.out.println("Enter your credit card number: ");
+		System.out.println("Enter your credit card number or -99 to stop: ");
 		String ccNum = in.next();
 		
 		int i = 0;
 		
-		while (i < ccNum.length()) {
-			char ch = ccNum.charAt(i);
-			if (ch == ' ' || ch == '-') {
+		while (in.hasNextLine()) {
+			ccNum = in.nextLine();
+			if (ccNum.equals("-99")) {
+				System.out.println("Goodbye");
+				break;
+			}
+			while (i < ccNum.length()) {
+				char ch = ccNum.charAt(i);
+				if (ch == ' ' || ch == '-') {
 				String before = ccNum.substring(0, i);
 				String after = ccNum.substring(i + 1);
 				ccNum = before + after;
 				
-			}
-			else {
+				}
+				else {
 				i++;
 			}
 		}
-		System.out.println(ccNum);
-			
-		
-		
+			System.out.println(ccNum);
+			System.out.println("Enter your credit card number or -99 to stop: ");
+	}
 	}
 
 }
